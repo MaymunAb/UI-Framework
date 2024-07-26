d5becc10b62add34c57f7124a8b86ea7ce2e3d3a
ArdaT <127962996+MaymunAb@users.noreply.github.com>
2024-07-26 12:41:18 +0300

    Delete Tween.lua

--- Tween.lua
+++
@@ -1,9 +0,0 @@
-local function tween(object, twINFO, goal)
-	if object and twINFO and goal then
-		local ts = game:GetService("TweenService")
-		local tween = ts:Create(object,twINFO,goal)
-		if ts and tween and goal then
-			return tween
-		end
-	end
-end
